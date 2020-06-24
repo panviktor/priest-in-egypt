5fc5f18c35c3fb702f86e77f78a5eead2a235740
SwiftCoder <SwiftCoder@nossvwpdf.ls>
2020-06-24 14:45:36 +0300

    Initial Commit

---
+++ Priest in Egypt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Priest in Egypt
+//
+//  Created by Viktor on 24.06.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
